69811ab1b198c0dd23df0226b1d37acc266774dd
Yemi Oliyide <oliyide@gmail.com>
2022-06-20 12:17:15 +0100

    listdrives, listrides and more done

--- app/Http/Controllers/RideController.php
+++ app/Http/Controllers/RideController.php
@@ -2,31 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ride;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class RideController extends Controller
 {
     /**
      *  Action for administrator to list drivers' drives by driver
      */
-    public function listDrives()
+    public function listDrives($driverId)
     {
+        // let's grab the Ride Model
+        $ride = new Ride();
+        $driverDrives = $ride::where('driver_id', "=", $driverId);
 
+        // we pass the list of the driver's drives to the view for display
+        return view('admin.listdrives', $driverDrives);
     }
 
     /**
      *  Action for Administrator to list riders' rides by rider
      */
-    public function listRides()
-    {
+    public function listRides($riderId)
+    {        
+        // let's grab the Ride Model
+        $ride = new Ride();
+        $riderRides = $ride::where('rider_id', "=", $riderId);
 
+        // We pass the list of the rider's rides to the view for display
+        return view('admin.listrides', $riderRides);
     }
 
     /**
      *  Action for a Rider to order a ride
      */
-    public function orderRide()
+    public function orderRide(Request $request)
     {
-        
+        // for a rider to other a ride the must be logged in to show they are registed
+        if(Session::has('loginId')){
+            $ride = new Ride();
+            $ride->driver_id = $request->driver_id;
+            $ride->rider_id = $request->rider_id;
+            $ride->pickup_location = $request->pickup_location;
+            $ride->destination_location = $request->destination_location;
+
+            // lets store the rides details in the database
+            $result = $ride->save();
+
+            // let's confirm the ride order status to use
+            if($result){
+                return back()->with('success', 'You have successfully order a ride');
+            }else{
+                return back()->with('fail', 'We have been unable to fulfill your order for a ride');
+            }
+        }
     }
 }
--- resources/views/admin/addvehicle.blade.php
+++ resources/views/admin/addvehicle.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Users Login</title>
+    <title>Add Vehicle</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
@@ -11,30 +11,45 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 col-md-offset-4" style="margin-top: 20px">
-            <h1>User Login</h1>
+            <h1>Add Vehicle To Company Fleet</h1>
             <hr>
-            <form action="{{route('login-user')}}" method="post">
-              @if (Session::has('success'))
-                <div class="alert alert-success">{{Session::get('success')}}</div>
-              @endif
-              @if (Session::has('fail'))
+            <form action="{{route('submit-registration')}}" method="post">
+                @if (Session::has('success'))
+                    <div class="alert alert-success">{{Session::get('success')}}</div>
+                @endif
+                @if (Session::has('fail'))
                 <div class="alert alert-success">{{Session::get('fail')}}</div>
-              @endif
-              @csrf
-              <div class="form-group">
-                  <label for="email">Email</label>
-                  <input type="email" class="form-control" placeholder="Enter Email" name="email">
-                  <span class="text-danger">@error('email') {{$message}} @enderror</span>
-              </div>               
-              <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" name="password">
+                @endif
+                @csrf
+                <div class="form-group">
+                    <label for="make">Vehical Make</label>
+                    <input type="text" class="form-control" placeholder="Enter Vehicle Model" name="make">
+                    <span class="text-danger">@error('make') {{$message}} @enderror</span>
+                </div>
+                <div class="form-group">
+                    <label for="model">Vehicle Model</label>
+                    <input type="text" class="form-control" placeholder="Enter Vehicle Model" name="model">
+                    <span class="text-danger">@error('model') {{$message}} @enderror</span>
+                </div>
+                <div class="form-group">
+                    <label for="year">Vehicle year</label>
+                    <input type="number" class="form-control" placeholder="Enter Vehicle Year" name="year">
+                    <span class="text-danger">@error('email') {{$message}} @enderror</span>
+                </div>                
+                <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input type="tel" class="form-control" placeholder="Enter Phone" name="phone">
+                    <span class="text-danger">@error('phone') {{$message}} @enderror</span>
+                </div>                
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password">
                     <span class="text-danger">@error('password') {{$message}} @enderror</span>
-              </div>                
-              <div class="form-group" style="margin-top: 10px">
-                  <button class="btn btn-block btn-primary" type="submit">Login</button>
-              </div>
-              Not Registered? <a href="/register">Click here</a>
+                </div>                
+                <div class="form-group" style="margin-top: 10px">
+                    <button class="btn btn-block btn-primary" type="submit">Register</button>
+                </div>
+                Already Registered? <a href="/login">Login here</a>
             </form>
         </div>
       </div>
